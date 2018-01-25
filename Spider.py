e0a79c3ecca4183d253b5a1194c4491fbea97507
Gunpowder <ccp750707@126.com>
2018-01-26 06:15:03 +0800

    Initial commit

---
+++ Spider.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import xlwt
+
+
+def get_page_soup(url):
+    driver.get(url)
+    html = driver.page_source
+    soup = BeautifulSoup(html, "lxml")
+    return soup
+
+
+def game_spider(url, bundle_name):
+    res = []
+    soup = get_page_soup(url)
+    games_tags = soup.find_all("p", class_="text-truncated m-0")
+    print("find %d game(s) in" % len(games_tags), bundle_name)
+    name = [x.contents[1] for x in games_tags]
+    price = [x.contents[4].text for x in games_tags]
+    for a, b in zip(name, price):
+        res.append([a, b])
+    return res
+
+
+def bundle_spider(url):
+    res = []
+    soup = get_page_soup(url)
+    bundle_tags = soup.find_all('a', class_="faux-block-link__overlay-link")
+    print("find %d bundle(s)" % len(bundle_tags))
+    prices_tag = soup.find_all('div', class_=' hitCardStripe d-flex justify-content-end' +
+                                             ' justify-content-md-between align-items-center card-block')
+    price = [x.contents[1].next.text for x in prices_tag]
+    name = [x.next.next for x in bundle_tags]
+    burl = [Fanatical + x['href'] for x in bundle_tags]
+    game = [game_spider(x, y) for x, y in zip(burl, name)]
+    for a, b, c, d in zip(name, price, burl, game):
+        res.append([a, b, c, d])
+    return res
+
+
+def output_to_excel():
+    wbk = xlwt.Workbook()
+    bundle_sheet = wbk.add_sheet("Bundle")
+    game_sheet = wbk.add_sheet("Game")
+    bundle_sheet.col(0).width = 9000
+    bundle_sheet.col(1).width = 6000
+    bundle_sheet.col(2).width = 18000
+    game_sheet.col(0).width = 12000
+    game_sheet.col(1).width = 6000
+
+    cols = 0
+    for bundle in Bundles:
+        bundle_sheet.write(cols, 0, bundle[0])
+        bundle_sheet.write(cols, 1, bundle[1])
+        bundle_sheet.write(cols, 2, bundle[2])
+        cols += 1
+
+    cols = 0
+    for bundle in Bundles:
+        game_sheet.write(cols, 0, bundle[0])
+        cols += 1
+        for game in bundle[3]:
+            game_sheet.write(cols, 0, game[0])
+            game_sheet.write(cols, 1, game[1])
+            cols += 1
+        cols += 2
+
+    wbk.save("D:\\1.xls")
+
+
+Fanatical = "https://www.fanatical.com"
+BundlePage = "https://www.fanatical.com/en/bundle"
+options = webdriver.FirefoxOptions()
+options.add_argument('-headless')
+driver = webdriver.Firefox(firefox_options=options)
+
+Bundles = bundle_spider(BundlePage)
+output_to_excel()
+driver.close()
